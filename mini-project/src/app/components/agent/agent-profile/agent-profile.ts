b8883c03ae735269ad0f843919b5639f9d00511a
poojithabejugam13 <poojithabejugam13@gmail.com>
2026-01-27 10:22:34 +0530

    agent-component-agent-dashboard-claims-profile

--- mini-project/src/app/components/agent/agent-profile/agent-profile.ts
+++ mini-project/src/app/components/agent/agent-profile/agent-profile.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AgentService } from '../../../services/agents';
+import { Auth } from '../../../services/auth';
 
 @Component({
+  standalone: true,
   selector: 'app-agent-profile',
-  imports: [],
-  templateUrl: './agent-profile.html',
-  styleUrl: './agent-profile.css',
+  imports: [CommonModule],
+  templateUrl: './agent-profile.html'
 })
 export class AgentProfile {
 
+  user= signal<any>([]);
+  agent=signal<any>([]);
+
+  constructor(
+    private auth: Auth,
+    private agentService: AgentService,
+    private http: HttpClient
+  ) {
+    this.loadUser();
+    this.loadAgent();
+  }
+
+  loadUser() {
+    const userId = String(this.auth.user()?.id);
+
+    this.http.get<any[]>('http://localhost:3000/users')
+      .subscribe(users => {
+        this.user.set(users.find(u => String(u.id) === userId));
+      });
+  }
+
+  loadAgent() {
+    this.agentService.getAgent((agent: any) => {
+      this.agent.set(agent);
+    });
+  }
 }
